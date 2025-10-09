b78c0d1478d4a3a64bc513d02589e814eab4ed2c
sahil-darj <darjisahil46@gmail.com>
2025-10-09 20:49:26 +0530

    Updated project code and fixed bugs

--- auth-app/server.js
+++ auth-app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
-
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+dotenv.config(); // Load environment variables from .env file
 
 dotenv.config();
 const app = express();
